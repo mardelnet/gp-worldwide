ded35b2157c0d64f5b91e9f86193138a59d06d6e
Pedro Figueroa <figueroash@gmail.com>
2024-11-15 15:19:17 +0100

    Makes the date filter work

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,32 @@ import {urls, api, nros} from "./constants";
 export default function Home() {
   const [posts, setPosts] = useState(null);
   const [country, setCountry] = useState(["international"]);
+  const [date, setDate] = useState("week");
+
+  function convertDates() {
+    const today = new Date();
+  
+    if (date === "day") {
+      today.setDate(today.getDate() - 1);
+    } else if (date === "week") {
+      today.setDate(today.getDate() - 7);
+    } else if (date === "month") {
+      today.setDate(today.getDate() - 30);
+    } else {
+      today.setDate(today.getDate() - 365);
+    }
+  
+    // Format the date to the desired format "YYYY-MM-DDT23:59:59"
+    const formattedDate = today.toISOString().split("T")[0] + "T23:59:59";
+  
+    return formattedDate;
+  }
 
   useEffect(() => {
     async function fetchPosts() {
       try {
-        // Create an array of fetch promises for each country
         const fetchPromises = country.map((c) =>
-          fetch(`${urls.proxy}/?${urls.gp}/${c}/${api.posts}?per_page=5`).then((res) => res.json())
+          fetch(`${urls.proxy}/?${urls.gp}/${c}/${api.posts}?after=${convertDates()}&per_page=100`).then((res) => res.json())
         );
   
         // Use Promise.all to fetch all data concurrently
@@ -30,7 +49,7 @@ export default function Home() {
     if (country.length > 0) {
       fetchPosts();
     }
-  }, [country]);
+  }, [country, date]);
   
   return (
     <div className="main-container">
@@ -49,13 +68,20 @@ export default function Home() {
             <h3>
               News filters:
             </h3>
-            <label htmlFor="campaign">Campaign:</label>
+            <label htmlFor="date">Date:</label>
+            <select id="date" onChange={(e) => setDate(e.target.value)}>
+              <option value="day">Last 24 hours</option>
+              <option value="week" selected>Last week</option>
+              <option value="month">Last month</option>
+              <option value="year">Last year</option>
+            </select>
+            {/* <label htmlFor="campaign">Campaign:</label>
             <select>
               <option>All</option>
               <option>Climate</option>
               <option>Biodiversity</option>
               <option>Social and Economic</option>
-            </select>
+            </select> */}
             <label htmlFor="country">Country:</label>
             <div className='countries'>
               {nros.map((nro) => (
@@ -85,15 +111,12 @@ export default function Home() {
             </div>
           </div>
           <Image
-            src="/deepgreen150.gif"
+            src="/monthinpicsmarch07.gif"
             width={150}
-            height={220}
+            height={150}
             alt="Greenpeace Image"
             priority
           />
-          <div>
-            You're the visitor #7789997
-          </div>
         </aside>
         <main>
           {
@@ -102,7 +125,7 @@ export default function Home() {
           {
             posts && (
               <>
-                <h1>News from {country.join(", ")}:</h1>
+                <h1>Showing {posts.length} news of the last {date} from {country.join(", ")}:</h1>
                 {posts.map((post, index) => (
                   <div key={index} className="post">
                     <div className="post-data">
@@ -119,13 +142,16 @@ export default function Home() {
           }
         </main>
         <aside>
-          <Image
-            src="/monthinpicsmarch07.gif"
+        <Image
+            src="/deepgreen150.gif"
             width={150}
-            height={150}
+            height={220}
             alt="Greenpeace Image"
             priority
           />
+          <div>
+            You're the visitor #7789997
+          </div>
           <Image
             src="/donations.gif"
             width={148}
